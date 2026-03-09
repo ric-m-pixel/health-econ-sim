3bbc7f5e5bf559a9a77f5321b10c73fa0e9540e8
ric-m-pixel <richa.mishra.tsk@gmail.com>
2026-03-09 19:32:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -150,9 +150,9 @@ if st.button("🚀 Run Full Decision Analysis", use_container_width=True):
             st.download_button("📥 Download Raw Data", df.to_csv().encode('utf-8'), "hta_data.csv")
     
         # Metrics
-    st.divider()
-    avg_icer = np.mean(inc_c) / np.mean(inc_o)
-p_ce = np.mean(inc_c < (inc_o * wtp)) * 100
-m1, m2 = st.columns(2)
-m1.metric("Mean ICER", f"₹{avg_icer:,.0f}")
-m2.metric("Confidence in Cost-Effectiveness", f"{p_ce:.1f}%")
+        st.divider()
+        avg_icer = np.mean(inc_c) / np.mean(inc_o)
+        p_ce = np.mean(inc_c < (inc_o * wtp)) * 100
+        m1, m2 = st.columns(2)
+        m1.metric("Mean ICER", f"₹{avg_icer:,.0f}")
+        m2.metric("Confidence in Cost-Effectiveness", f"{p_ce:.1f}%")
